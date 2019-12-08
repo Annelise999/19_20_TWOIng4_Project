39a68bfc037b0e5a4d0cc1faa7de73d9e5f9b384
Anne-Lise Hervé <anne-liseherve@MacBook-Air-de-Anne-Lise.local>
2019-12-08 17:38:28 +0100

    Model back

---
+++ backend/model/Sensors.model.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+mongoose.set('useCreateIndex', true);
+
+const SensorsSchema = new Schema({  
+    nom: {
+      type: String,
+      required: true
+    },
+    valeur: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true // Saves createdAt and updatedAt as dates. createdAt will be our timestamp.
+  });
+  
+  module.exports = mongoose.model('Sensors', SensorsSchema);  
+
